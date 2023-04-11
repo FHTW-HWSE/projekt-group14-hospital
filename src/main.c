3c3c38b4e8fe0794c79b162317e43d5b6ded0ba6
valentinaskn <valentina.saykin@gmail.com>
2023-04-11 18:31:45 +0200

    commit

--- src/main.c
+++ src/main.c
@@ -5,11 +5,21 @@
 
 int main (int argc, char *argv[]){
 
-char buffer[1000] = {0};
-
-buffer = read_csv("test.csv");
-
-printf("%s", buffer);
+ char* csv_content;
+    
+    // call read_csv-function and safe string 
+    csv_content = read_csv("test.csv");
+    
+    if (csv_content == NULL) {
+        printf("Error reading file\n");
+        return 1;
+    }
+    
+    // print contant of CSV
+    printf("CSV-content:\n%s\n", csv_content);
+    
+    // free allocated memory
+    free(csv_content);
 
     return 0;
 }
