58a98f7bd075222531cb9878eb85938845169047
Emil Atic <ic22b005@technikum-wien.at>
2023-04-17 20:20:27 +0200

    getDate and getTime

--- src/dateFunctions.cpp
+++ src/dateFunctions.cpp
@@ -1,2 +1,38 @@
 #include "../include/dateFunctions.h"
 #include <time.h>
+#include <stdio.h>
+
+
+int getTime() {
+    time_t now;
+    int hour, minute, result;
+    struct tm *timeInfo;
+
+    time(&now);
+    timeInfo = localtime(&now);
+    hour = timeInfo->tm_hour;
+    minute = timeInfo->tm_min;
+    hour = hour*100;
+    result = hour+minute;
+
+    return result;
+}
+
+long getDate() {
+    time_t now;
+    struct tm *timeInfo;
+    long date=0;
+    int year, month, day;
+
+    time(&now);
+    timeInfo = localtime(&now);
+    year = timeInfo->tm_year; //year since 1900
+    month = timeInfo->tm_mon; //month 0-11
+    day = timeInfo->tm_mday; //day
+
+    year=(year+1900)*10000;
+    month=(month+1)*100;
+
+    date = year+month+day;
+    return date;
+}
