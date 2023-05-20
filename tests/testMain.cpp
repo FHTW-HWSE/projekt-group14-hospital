9bf73946bbee6a87223261b2ea88a812475cdf88
michi <michalkuc55@gmail.com>
2023-05-20 17:58:53 +0200

    Cmake verändert

--- tests/testMain.cpp
+++ tests/testMain.cpp
@@ -1,13 +1,2 @@
-#include <catch2/catch.hpp>;
-
-#if 0
-int main( int argc, char* argv[] ) {
-  // global setup...
-
-  int result = Catch::Session().run( argc, argv );
-
-  // global clean-up...
-
-  return result;
-}
-#endif
+#include <catch2/catch.hpp>
+#define CATCH_CONFIG_MAIN
