b206981092162aed5390f997a3edd8d8e51c13e2
valentina.skn <valentina.saykin@gmail.com>
2023-06-08 21:39:37 +0200

    fixes in cmake and test file

--- tests/dateFunctions_tests.cpp
+++ tests/dateFunctions_tests.cpp
@@ -1,6 +1,6 @@
 
 #include <catch2/catch.hpp>
-#include ../include/dateFunctions.h"
+#include "../include/dateFunctions.h"
 
 TEST_CASE("Time_returned_in_hhmm_format_as_int") {
     int test=0, time=0;
