3c0fee8009b8d474750fe177a7537aa63dbdc568
Emil Atic <ic22b005@technikum-wien.at>
2023-06-11 22:37:39 +0200

    fixes

--- tests/dateFunctions_tests.cpp
+++ tests/dateFunctions_tests.cpp
@@ -5,11 +5,13 @@
 TEST_CASE("Time_returned_in_hhmm_format_as_int") {
     int result = getTime();
 
-    REQUIRE(result <= 2359 && result >=0);
+    REQUIRE(result <= 2359);
+    REQUIRE(result >= 0);
 }
 
 TEST_CASE("Time_returned_in_yyyymmdd_format_as_int") {
     int result = getDate();
 
-    REQUIRE(result <= 99991231 && result >=20230101);
+    REQUIRE(result <= 99991231);
+    REQUIRE(result >=20230101);
 }
